be070915c1ae45cc9b199e18d016361f39321825
pawuload <adamarekpawel@gmail.com>
2022-07-17 12:58:55 +0200

    lol

---
+++ lib/models/video.dart
@@ -0,0 +1,15 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'video.freezed.dart';
+
+part 'video.g.dart';
+
+@freezed
+class Video with _$Video {
+  factory Video({
+    required String thumbnail,
+    required String videoUrl,
+}) = _Video;
+
+  factory Video.fromJson(Map<String, dynamic> json) => _$VideoFromJson(json);
+}
