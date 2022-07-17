88121934dc74508bdd624ba9f9ee91177fa29487
pawuload <adamarekpawel@gmail.com>
2022-07-17 13:09:12 +0200

    add } in video service (XD)

--- lib/service/video_service.dart
+++ lib/service/video_service.dart
@@ -1,15 +1,16 @@
-//
-// import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'package:walczak/models/video.dart';
-//
-// class ItemService {
-//
-//   Future<List<Video>> getAllItems() async {
-//     final CollectionReference collection = FirebaseFirestore.instance.collection('video/');
-//
-//     final result = await collection.get();
-//     final value = result.docs.map(
-//           (e) => Video.fromJson(e.data())
-//     );
-//     return value.toList();
-//   }
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:walczak/models/video.dart';
+
+class VideoService {
+
+  Future<List<Video>> getAllItems() async {
+    final CollectionReference collection = FirebaseFirestore.instance.collection('video/');
+
+    final result = await collection.get();
+    final value = result.docs.map(
+            (e) => Video.fromJson(e.data())
+    );
+    return value.toList();
+  }
+}
